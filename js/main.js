6827d2e1ba5ce660dd82e90fccc746a310320baf
MohdAlmosawy <mohd.s.aqeel@gmail.com>
2023-07-09 14:17:09 +0300

    drawing the circle as i want but not correctly

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-const textElement = document.getElementById('gameName');
-const text = textElement.textContent;
-const colorsPalette = ['rgb(255, 65, 54)', 'rgb(0, 116, 217)', 'rgb(46, 204, 64)', 'rgb(255, 220, 0)', 'white'];
-let styledText = '';
-for (let i = 0; i < text.length; i++) {
-  let colorIndex = i % colorsPalette.length;
-  const span = `<span style="color: ${colorsPalette[colorIndex]}">${text[i]}</span>`;
-  styledText += span;
-}
-textElement.innerHTML = styledText;
+// coloring the game title in main menu, un commit at end 
+
+// const textElement = document.getElementById('gameName');
+// const text = textElement.textContent;
+// const colorsPalette = ['rgb(255, 65, 54)', 'rgb(0, 116, 217)', 'rgb(46, 204, 64)', 'rgb(255, 220, 0)', 'white'];
+// let styledText = '';
+// for (let i = 0; i < text.length; i++) {
+//   let colorIndex = i % colorsPalette.length;
+//   const span = `<span style="color: ${colorsPalette[colorIndex]}">${text[i]}</span>`;
+//   styledText += span;
+// }
+// textElement.innerHTML = styledText;
