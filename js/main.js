6f3710cc6029efcd1aaa2ddf533a28d49b8a2cff
MohdAlmosawy <mohd.s.aqeel@gmail.com>
2023-07-11 18:52:09 +0300

    solving the User Input defect using userTurn and myTimer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 // coloring the game title in main menu
-const textElement = document.getElementById('gameName');
+const textElement = document.getElementById("gameName");
 const text = textElement.textContent;
-const colorsPalette = ['rgb(255, 65, 54)', 'rgb(0, 116, 217)', 'rgb(46, 204, 64)', 'rgb(255, 220, 0)', 'white'];
-let styledText = '';
+const colorsPalette = [
+  "rgb(255, 65, 54)",
+  "rgb(0, 116, 217)",
+  "rgb(46, 204, 64)",
+  "rgb(255, 220, 0)",
+  "white",
+];
+let styledText = "";
 for (let i = 0; i < text.length; i++) {
   let colorIndex = i % colorsPalette.length;
   const span = `<span style="color: ${colorsPalette[colorIndex]}">${text[i]}</span>`;
@@ -13,22 +19,23 @@ textElement.innerHTML = styledText;
 // share the fun function
 document.getElementById("share").addEventListener("click", shareTheFun);
 function shareTheFun() {
-    const message = "Hey there! I just played this incredible game called Copycat Simons' Symphony. It's so addictive and challenging! You've got to check it out. Here's the link: [game link].";
-    
-    // Create a temporary input element
-    const input = document.createElement('input');
-    input.value = message;
-    document.body.appendChild(input);
-    
-    // Copy the message to clipboard
-    input.select();
-    document.execCommand('copy');
-    
-    // Remove the temporary input element
-    document.body.removeChild(input);
-    
-    // Display the toast notification
-    const toastHTML = `
+  const message =
+    "Hey there! I just played this incredible game called Copycat Simons' Symphony. It's so addictive and challenging! You've got to check it out. Here's the link: [game link].";
+
+  // Create a temporary input element
+  const input = document.createElement("input");
+  input.value = message;
+  document.body.appendChild(input);
+
+  // Copy the message to clipboard
+  input.select();
+  document.execCommand("copy");
+
+  // Remove the temporary input element
+  document.body.removeChild(input);
+
+  // Display the toast notification
+  const toastHTML = `
       <div id="notification">
         <div class="notification-container" aria-live="polite" aria-atomic="true">
           <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true">
@@ -43,22 +50,24 @@ function shareTheFun() {
         </div>
       </div>
     `;
-    
-    // Insert the toast notification HTML into the "notification" div
-    const notificationDiv = document.getElementById("notification");
-    notificationDiv.innerHTML = toastHTML;
-    
-    // Add position: fixed to the .notification-container
-    const notificationContainer = notificationDiv.querySelector('.notification-container');
-    if (notificationContainer) {
-      notificationContainer.style.position = 'fixed';
-    }
-  
-    // Remove the toast notification 
-    setTimeout(function () {
-      notificationDiv.innerHTML = '';
-    }, 5000);
+
+  // Insert the toast notification HTML into the "notification" div
+  const notificationDiv = document.getElementById("notification");
+  notificationDiv.innerHTML = toastHTML;
+
+  // Add position: fixed to the .notification-container
+  const notificationContainer = notificationDiv.querySelector(
+    ".notification-container"
+  );
+  if (notificationContainer) {
+    notificationContainer.style.position = "fixed";
   }
+
+  // Remove the toast notification
+  setTimeout(function () {
+    notificationDiv.innerHTML = "";
+  }, 5000);
+}
 
 ////////////////////////////////////////////////////////////////// screen visability logic
 
@@ -90,24 +99,24 @@ function startCounterAndShowGame() {
   countdownScreen.style.visibility = "visible";
 
   // Play the countdown sound
-  const audio = new Audio('assets/Audio/simpleGameCountdown.wav');
+  const audio = new Audio("assets/Audio/simpleGameCountdown.wav");
   audio.play();
 
   // Define the countdown numbers
-  const countDownArray = ['3','2','1', 'Go'];
+  const countDownArray = ["3", "2", "1", "Go"];
 
   // Define a separate function for the setTimeout logic
   function runCountdown(index) {
     setTimeout(() => {
       // Update the text inside countDownNumbers
-      const countDownElement = document.getElementById('countDownNumbers');
+      const countDownElement = document.getElementById("countDownNumbers");
       countDownElement.textContent = countDownArray[index];
 
       // Hide the countDownScreen after the last countdown number
       if (index === countDownArray.length - 1) {
-        countdownScreen.style.visibility = 'hidden';
-          // Start the game by calling addAndPlay initially
-          addAndPlay();
+        countdownScreen.style.visibility = "hidden";
+        // Start the game by calling addAndPlay initially
+        addAndPlay();
       }
     }, (index + 1) * 1000); // Delay between countdown numbers
   }
@@ -116,8 +125,6 @@ function startCounterAndShowGame() {
   for (let i = 0; i < countDownArray.length; i++) {
     runCountdown(i);
   }
-
-
 }
 
 //////////////////// Capturing and validating player input against the generated sequences.
@@ -126,14 +133,20 @@ let gameStatus = true;
 let currentScore = 0;
 let sequence = [];
 let userSeq = [];
+let userTurn = false;
+console.log("initial status user turn: " + userTurn);
+let myTimer = 0;
 
 function addAndPlay() {
   const randomChoice = Math.floor(Math.random() * 4) + 1;
   sequence.push(randomChoice);
   playSequence();
-  setUpEventListener();
 }
+
 function playSequence() {
+  userTurn = false;
+  console.log("playSequence started and user turn: " + userTurn);
+  myTimer = 0; // Reset the timer for each sequence playback
   for (let i = 0; i < sequence.length; i++) {
     const quarterElement = document.getElementById(`quarter${sequence[i]}`);
     setTimeout(() => {
@@ -143,14 +156,36 @@ function playSequence() {
       setTimeout(() => {
         quarterElement.style.filter = "";
       }, 500);
-    }, 700 * (i + 1));
+    }, myTimer + 700); // Delay based on the accumulated timer value
+    myTimer += 700; // Accumulate the delay for the next element
   }
+  setTimeout(() => {
+    userTurn = true;
+    console.log("playSequence ended and user turn: " + userTurn);
+    setUpEventListener();
+  }, myTimer + 700); // Delay before enabling user input
 }
 
 function setUpEventListener() {
   const quarters = document.getElementsByClassName("quarter");
   for (let i = 0; i < quarters.length; i++) {
     quarters[i].addEventListener("click", handleUserInput);
+  }
+}
+
+function handleUserInput(event) {
+  if (gameStatus && userTurn) {
+    const clickedQuarter = event.target;
+    const quarterIndex = +clickedQuarter.id.slice(-1); // Using unary plus operator for conversion
+    userSeq.push(quarterIndex);
+    console.log("User input:", userSeq); // Added console log
+    compareSequences();
+    // Play audio for the clicked quarter
+    const audio = new Audio(`assets/Audio/sound${quarterIndex}.mp3`);
+    audio.play();
+  } else {
+    // Ignore user input when it's not their turn
+    console.log("Not the user's turn");
   }
 }
 
@@ -170,7 +205,6 @@ function compareSequences() {
     gameStatus = false;
     // Game over, show the score screen or perform other actions
     showScoreScreen();
-    
   }
 }
 
@@ -193,31 +227,17 @@ function checkHighScore() {
   const highScoreElements = document.querySelectorAll(".highScore");
 
   // Update the currentScore elements
-  currentScoreElements.forEach(element => {
+  currentScoreElements.forEach((element) => {
     element.textContent = currentScore;
   });
 
   // Update the highScore elements if necessary
-  highScoreElements.forEach(element => {
+  highScoreElements.forEach((element) => {
     const highScore = element.textContent;
     if (currentScore > highScore) {
       element.textContent = currentScore;
     }
   });
-}
-
-
-function handleUserInput(event) {
-  if (gameStatus) {
-    const clickedQuarter = event.target;
-    const quarterIndex = parseInt(clickedQuarter.id.slice(-1));
-    userSeq.push(quarterIndex);
-    console.log("User input:", userSeq); // Added console log
-    compareSequences();
-    // Play audio for the clicked quarter
-    const audio = new Audio(`assets/Audio/sound${quarterIndex}.mp3`);
-    audio.play();
-  }
 }
 
 // Function to reset the game state
